c72bf5ed7e5b151b9d9ebd2ec35fbb30da23686b
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-10-29 07:29:13 +0000

    Commented mysql connection

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,16 @@
-// // MySQL connection
-let mysql = require('mysql2');
+// // // MySQL connection
+// let mysql = require('mysql2');
 
-let connection = mysql.createConnection({
-    user: "root",
-    password: "my-secret-pw",
-    database: "uber",
-    multipleStatements: true //when multiple queries are executed at the same time
-});
+// let connection = mysql.createConnection({
+//     user: "root",
+//     password: "my-secret-pw",
+//     database: "uber",
+//     multipleStatements: true //when multiple queries are executed at the same time
+// });
 
-connection.connect(err => {
-    if(err) throw err
-    console.log("Database connected")
-})
+// connection.connect(err => {
+//     if(err) throw err
+//     console.log("Database connected")
+// })
 
-module.exports = connection;
+// module.exports = connection;
